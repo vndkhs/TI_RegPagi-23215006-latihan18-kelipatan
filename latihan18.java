568666d057e1073d69eb9f17c23be9d89ab4b4db
vndkhs <sitinoviadesi@gmail.com>
2024-11-04 17:03:05 +0700

    Add files via upload

---
+++ latihan18.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class latihan18 {
+    public static void main(String[] args) {
+        // Mulai dari 3.5, kelipatan 3.5
+        double angka = 3.5;
+        
+        // Looping dari 3.5 sampai dengan 35.0
+        while (angka <= 35.0) {
+            System.out.println(angka);
+            angka += 3.5; // Tambahkan 3.5 setiap iterasi
+        }
+    }
+}
+
+
